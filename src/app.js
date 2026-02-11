acb4382ffcd21ed0efe9d1412957b532435e06c1
sidneyemeka <nnaemekasidney@gmail.com>
2026-02-11 19:00:13 +0100

    start db async

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ const startDb = require('./configs/dbconnect.js');
 const authRoutes = require('./routes/authroutes.js');
 const userRoutes = require('./routes/userroute.js')
 
-startDb();
+startDb().then((r)=>{
+  if(r==true){
+    app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`)
+ });
+  }
+});
 
 
 const app = express()
@@ -20,9 +26,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`)
- });
+
 
 app.get('/health', (req, res) => {
   res.status(200).send({
--- src/configs/dbconnect.js
+++ src/configs/dbconnect.js
@@ -4,9 +4,12 @@ const dbConnect = async() => {
 try{
     const connect = await mongoose.connect(`${process.env.CONNECTION_STRING}`);
 console.log(`DB CONNECTED ${connect.connection.host}, ${connect.connection.name},`);
+return true;
 
 }catch(err){
   console.log(`connection no work ooo ${err}`)
+  return false;
+
 };
 
 }
