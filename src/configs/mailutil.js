7dc03876ad5fc37ae0b4e88a41be424e290f63df
sidneyemeka <nnaemekasidney@gmail.com>
2026-03-07 03:50:48 +0100

    brevo

--- src/configs/mailutil.js
+++ src/configs/mailutil.js
@@ -1,10 +1,21 @@
 const nodemailer = require('nodemailer');
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail', 
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS, 
+//   },
+// });
+
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, 
+    user: process.env.BREVO_USER,   
+    pass: process.env.BREVO_PASS,  
   },
 });
 
